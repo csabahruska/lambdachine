b07eaf23beeb0f050e33c5430c8783f02a7b4609
Csaba Hruska <csaba.hruska@gmail.com>
2019-02-26 11:57:06 +0100

    fix setup scripts to work with stack

--- build-setup.sh
+++ build-setup.sh
@@ -1,7 +1,16 @@
 set -x -e
 
+# STEP 1: setup autoconf with stack's sandboxed GHC
+
+stack exec -- ./boot
+stack exec -- ./configure
+
+
+# STEP 2: generate lamdachine's special files
+
 c++ utils/genopcodes.cc -I vm -o genopcodes
 
 ./genopcodes > compiler/Opcodes.h
 
-echo "#define PACKAGE_CONF_STRING \"$(PWD)/libraries/package.conf\"" > lcc/Locations.h
+PWD=`pwd`
+echo "#define PACKAGE_CONF_STRING \"${PWD}/libraries/package.conf\"" > lcc/Locations.h
